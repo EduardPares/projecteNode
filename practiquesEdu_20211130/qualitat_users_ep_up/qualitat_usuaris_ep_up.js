de049956e880ddd321b9f4bb15d05b90b191b3a8
EduardPares <eduardparesllobet@gmail.com>
2021-12-02 12:51:26 +0100

    Exercicis Albert. Buits.

---
+++ practiquesEdu_20211130/qualitat_users_ep_up/qualitat_usuaris_ep_up.js
@@ -0,0 +1,105 @@
+const fs = require('fs');
+
+const usersAll = JSON.parse(fs.readFileSync('./users.js', 'utf8')); 
+
+// ======================================================
+function prettyJ(json) {
+  if (typeof json !== 'string') {
+    json = JSON.stringify(json, undefined, 2);
+  }
+  return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, 
+    function (match) {
+      let cls = "\x1b[36m";
+      if (/^"/.test(match)) {
+        if (/:$/.test(match)) {
+          cls = "\x1b[34m";
+        } else {
+          cls = "\x1b[32m";
+        }
+      } else if (/true|false/.test(match)) {
+        cls = "\x1b[35m"; 
+      } else if (/null/.test(match)) {
+        cls = "\x1b[31m";
+      }
+      return cls + match + "\x1b[0m";
+    }
+  );
+}
+// ======================================================
+
+console.log("--------------------------------------------------------------------------------------------------------------");
+console.log("Llistat dels usuaris i els àmbits (EP,UP) on estan assignats");
+usersAll.forEach(function (unUser) {
+    const userAmbit = unUser.ambit;
+	console.log(unUser.dades.nom);
+	console.log(unUser.dades.cognoms);
+	unUser.ambit.forEach(function (unAmbit) {
+		const [ep,up] = unAmbit.split('_');
+		console.log("--> "+[ep,up]);
+	});
+});
+console.log("--------------------------------------------------------------------------------------------------------------");
+console.log("FORMAT JSON. Llistat dels usuaris i els àmbits (EP,UP) on estan assignats");
+var usuarisAmbits = [];
+usersAll.forEach(function (unUser) {
+    const userAmbit = unUser.ambit;
+	var userResultat = {nom:unUser.dades.nom, cognoms:unUser.dades.cognoms, ambit:[]}
+	unUser.ambit.forEach(function (unAmbit) {
+		const [ep,up] = unAmbit.split('_');
+		console.log("--> "+[ep,up]);
+		userResultat.ambit.push({ep:ep,up:up});
+	});
+	usuarisAmbits.push(userResultat);
+});
+console.log(prettyJ(usuarisAmbits));
+
+console.log("--------------------------------------------------------------------------------------------------------------");
+console.log("FORMAT JSON: Llistat de les EP i la quantitat de UP");
+var  resumEP = {};
+usersAll.forEach(function (unUser) {
+	const userAmbit = unUser.ambit;
+	unUser.ambit.forEach(function (unAmbit) {
+		const [ep,up] = unAmbit.split('_');
+		if (!resumEP[ep]) {
+			resumEP[ep]={};
+		}
+		if (!resumEP[ep][up]) {
+			resumEP[ep][up]=0;
+		}
+		resumEP[ep][up] += 1;			
+	})	
+});
+console.log(prettyJ(resumEP));
+
+console.log("--------------------------------------------------------------------------------------------------------------");
+console.log("Llistat de les EP i les seves UP");
+
+console.log("--------------------------------------------------------------------------------------------------------------");
+console.log("Llistat de les EP i els usuaris que estan assignats");
+
+
+console.log("--------------------------------------------------------------------------------------------------------------");
+console.log("Llistat de les EP i la quantiat d'usuaris que estan assignats");
+
+
+
+
+console.log("--------------------------------------------------------------------------------------------------------------");
+console.log("Llistat de les EP i UP, i els usuaris que estan assignats");
+
+
+
+console.log("--------------------------------------------------------------------------------------------------------------");
+console.log("Llistat dels usuaris que comparteixen àmbit i què tenen en comú");
+
+
+
+console.log("--------------------------------------------------------------------------------------------------------------");
+console.log("Llistat dels usuaris sense àmbit assignat");
+
+
+
+
+
+
+
---
+++ practiquesEdu_20211130/qualitat_users_ep_up/users.js
@@ -0,0 +1,91 @@
+[{
+ "id": 1326153530,
+ "especialitat": [
+  "Psicologia",
+  "Psiquiatria"
+ ],
+ "password": "$2a$13$CqvmmNYbR0DMtK1xqWkXPePQpfoeZutwD1H0S0vWRQhMhqKm4stOy",
+ "actius": {
+  "general": false
+ },
+ "terapeutaid": "idTera2",
+ "dades": {
+  "nom": "Nom2",
+  "cognoms": "Cognom21 Cognom22"
+ },
+ "email": "adre2@correu.eu",
+ "rol": "terapeuta",
+ "ambit": [
+  "001_002",
+  "001_003"
+ ]
+},
+{
+ "id": 134444530,
+ "especialitat": [
+  "Psicologia",
+  "Psiquiatria"
+ ],
+ "password": "xxxxxxxxxxxxxxxxxxxxxxxxx",
+ "actius": {
+  "general": true
+ },
+ "terapeutaid": "idTera2",
+ "dades": {
+  "nom": "Nom2",
+  "cognoms": "Cognom21 Cognom22"
+ },
+ "email": "adre2@correu.eu",
+ "rol": "terapeuta",
+ "ambit": [
+  "001_002",
+  "001_003"
+ ]
+},
+{
+ "id": 221066623820,
+ "especialitat": [],
+ "password": "$2a$13$tGop.OUN2z7IuaEo7BU5pOB/uY.utPHmD8ubtDA1bsofq8iaNQ94K",
+ "actius": {
+  "general": false
+ },
+ "terapeutaid": "xxx",
+ "dades": {
+  "nom": "NOM asfdsafd",
+  "cognoms": "COGNOMS dsfadfasd fsdafasd"
+ },
+ "email": "ddddd@dddddd.org",
+ "rol": "terapeuta",
+ "ambit": [
+  "0152_03076"
+ ]
+},
+{
+ "id": 3223,
+ "especialitat": [
+  "psicologia",
+  "psiquiatria"
+ ],
+ "password": "$2a$13$WOBlL2K7p6LUtKbzazSsHejWqbsdnsBdjvROcwDAw27aIua0ebRoS",
+ "actius": {
+  "general": true,
+  "rebremails": false
+ },
+ "idioma": "ca",
+ "terapeutaid": "ccccccc",
+ "dades": {
+  "nom": "Nom: dfdasfasf",
+  "cognoms": "Cognoma: afasdfsdafsda"
+ },
+ "email": "vcvxz@zxcvxzvcxz.com",
+ "rol": [
+  "terapeuta",
+  "admin",
+  "manager"
+ ],
+ "ambit": [
+  "0082_04270",
+  "0082_04269"
+ ]
+}
+]
