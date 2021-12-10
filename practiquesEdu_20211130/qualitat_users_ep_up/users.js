acf7db86b4f6d4eea26ac5435d394ade0de48a60
EduardPares <eduardparesllobet@gmail.com>
2021-12-10 11:00:48 +0100

    Actualització users.js

--- practiquesEdu_20211130/qualitat_users_ep_up/users.js
+++ practiquesEdu_20211130/qualitat_users_ep_up/users.js
@@ -1,91 +1,166 @@
-[{
- "id": 1326153530,
- "especialitat": [
-  "Psicologia",
-  "Psiquiatria"
- ],
- "password": "$2a$13$CqvmmNYbR0DMtK1xqWkXPePQpfoeZutwD1H0S0vWRQhMhqKm4stOy",
- "actius": {
-  "general": false
- },
- "terapeutaid": "idTera2",
- "dades": {
-  "nom": "Nom2",
-  "cognoms": "Cognom21 Cognom22"
- },
- "email": "adre2@correu.eu",
- "rol": "terapeuta",
- "ambit": [
-  "001_002",
-  "001_003"
- ]
-},
-{
- "id": 134444530,
- "especialitat": [
-  "Psicologia",
-  "Psiquiatria"
- ],
- "password": "xxxxxxxxxxxxxxxxxxxxxxxxx",
- "actius": {
-  "general": true
- },
- "terapeutaid": "idTera2",
- "dades": {
-  "nom": "Nom23",
-  "cognoms": "Cognom213 Cognom223"
- },
- "email": "adre2@correu.eu",
- "rol": "terapeuta",
- "ambit": [
-  "001_002",
-  "001_003"
- ]
-},
-{
- "id": 221066623820,
- "especialitat": [],
- "password": "$2a$13$tGop.OUN2z7IuaEo7BU5pOB/uY.utPHmD8ubtDA1bsofq8iaNQ94K",
- "actius": {
-  "general": false
- },
- "terapeutaid": "xxx",
- "dades": {
-  "nom": "NOM asfdsafd",
-  "cognoms": "COGNOMS dsfadfasd fsdafasd"
- },
- "email": "ddddd@dddddd.org",
- "rol": "terapeuta",
- "ambit": [
-  "0152_03076"
- ]
-},
-{
- "id": 3223,
- "especialitat": [
-  "psicologia",
-  "psiquiatria"
- ],
- "password": "$2a$13$WOBlL2K7p6LUtKbzazSsHejWqbsdnsBdjvROcwDAw27aIua0ebRoS",
- "actius": {
-  "general": true,
-  "rebremails": false
- },
- "idioma": "ca",
- "terapeutaid": "ccccccc",
- "dades": {
-  "nom": "Nom: dfdasfasf",
-  "cognoms": "Cognoma: afasdfsdafsda"
- },
- "email": "vcvxz@zxcvxzvcxz.com",
- "rol": [
-  "terapeuta",
-  "admin",
-  "manager"
- ],
- "ambit": [
-  "0082_04270",
-  "0082_04269"
- ]
-}
+[
+
+  {
+    "id": 1326153530,
+    "especialitat": [
+      "Psicologia",
+      "Psiquiatria"
+    ],
+    "password": "$2a$13$CqvmmNYbR0DMtK1xqWkXPePQpfoeZutwD1H0S0vWRQhMhqKm4stOy",
+    "actius": {
+      "general": false
+    },
+    "terapeutaid": "idTera2",
+    "dades": {
+      "nom": "Nom2",
+      "cognoms": "Cognom21 Cognom22"
+    },
+    "email": "adre2@correu.eu",
+    "rol": "terapeuta",
+    "ambit": [
+      "001_002",
+      "001_003"
+    ]
+  },
+  {
+    "id": 132699883530,
+    "especialitat": [
+      "Psicologia",
+      "Psiquiatria"
+    ],
+    "password": "$2a$13$CqvmmNYbR0DMtK1xqWkXPePQpfoeZutwD1H0S0vWRQhMhqKm4stOy",
+    "actius": {
+      "general": false
+    },
+    "terapeutaid": "idTera2",
+    "dades": {
+      "nom": "Nom2hhhh",
+      "cognoms": "Cognom21 Cognom22"
+    },
+    "email": "adre2@correu.eu",
+    "rol": "terapeuta",
+    "ambit": [
+      "001_002",
+      "001_003",
+      "001_009"
+    ]
+  },
+  {
+    "id": 13499444530,
+    "especialitat": [
+      "Psicologia",
+      "Psiquiatria"
+    ],
+    "password": "xxxxxxxxxxxxxxxxxxxxxxxxx",
+    "actius": {
+      "general": true
+    },
+    "terapeutaid": "idTera2",
+    "dades": {
+      "nom": "Nom23",
+      "cognoms": "Cognom213 Cognom223"
+    },
+    "email": "adre2@correu.eu",
+    "rol": "terapeuta",
+    "ambit": []
+  },
+  {
+    "id": 134444530,
+    "especialitat": [
+      "Psicologia",
+      "Psiquiatria"
+    ],
+    "password": "xxxxxxxxxxxxxxxxxxxxxxxxx",
+    "actius": {
+      "general": true
+    },
+    "terapeutaid": "idTera2",
+    "dades": {
+      "nom": "Nom23",
+      "cognoms": "Cognom213 Cognom223"
+    },
+    "email": "adre2@correu.eu",
+    "rol": "terapeuta",
+    "ambit": [
+      "001_002",
+      "001_003"
+    ]
+  },
+  {
+    "id": 221066623820,
+    "especialitat": [],
+    "password": "$2a$13$tGop.OUN2z7IuaEo7BU5pOB/uY.utPHmD8ubtDA1bsofq8iaNQ94K",
+    "actius": {
+      "general": false
+    },
+    "terapeutaid": "xxx",
+    "dades": {
+      "nom": "NOM asfdsafd",
+      "cognoms": "COGNOMS dsfadfasd fsdafasd"
+    },
+    "email": "ddddd@dddddd.org",
+    "rol": "terapeuta",
+    "ambit": [
+      "0152_03076"
+    ]
+  },
+  {
+    "id": 2299623820,
+    "especialitat": [],
+    "password": "$2a$13$tGop.OUN2z7IuaEo7BU5pOB/uY.utPHmD8ubtDA1bsofq8iaNQ94K",
+    "actius": {
+      "general": false
+    },
+    "terapeutaid": "xxx",
+    "dades": {
+      "nom": "NOM asfdsafd",
+      "cognoms": "COGNOMS dsfadfasd fsdafasd"
+    },
+    "email": "ddddd@dddddd.org",
+    "rol": "terapeuta",
+    "ambit": []
+  },
+  {
+    "id": 3223,
+    "especialitat": [
+      "psicologia",
+      "psiquiatria"
+    ],
+    "password": "$2a$13$WOBlL2K7p6LUtKbzazSsHejWqbsdnsBdjvROcwDAw27aIua0ebRoS",
+    "actius": {
+      "general": true,
+      "rebremails": false
+    },
+    "idioma": "ca",
+    "terapeutaid": "ccccccc",
+    "dades": {
+      "nom": "Nom: dfdasfasf",
+      "cognoms": "Cognoma: afasdfsdafsda"
+    },
+    "email": "vcvxz@zxcvxzvcxz.com",
+    "rol": [
+      "terapeuta",
+      "admin",
+      "manager"
+    ]
+  },
+  {
+    "id": 221000623820,
+    "especialitat": [],
+    "password": "$2a$13$tGop.OUN2z7IuaEo7BU5pOB/uY.utPHmD8ubtDA1bsofq8iaNQ94K",
+    "actius": {
+      "general": false
+    },
+    "terapeutaid": "xxx",
+    "dades": {
+      "nom": "NOM ssddff",
+      "cognoms": "COGNOMS dsfadfasd fsdafasd"
+    },
+    "email": "ddddd@dddddd.org",
+    "rol": "terapeuta",
+    "ambit": [
+      "0152_03076",
+      "0152_03076"
+    ]
+  }
 ]
